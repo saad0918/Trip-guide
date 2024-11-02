80c82e3a368f277a347af8a1e94235258e1d1314
saad0918 <saadali7782@gmail.com>
2024-11-02 21:29:30 +0530

    all good

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-<script>
-document.getElementById("click").onclick(function(){
-    alert("Thank you !")
+let popup = document.getElementById("popup");
+
+function openPopup() {
+popup.classList.add("open-popup");
+}
+
+function closePopup() {
+popup.classList.remove("open-popup");
+}
+document.addEventListener("DOMContentLoaded", function() {
+let popup = document.getElementById("popup");
+
+window.openPopup = function() {
+popup.classList.add("open-popup");
+}
+
+window.closePopup = function() {
+popup.classList.remove("open-popup");
+}
 });
-</script>
